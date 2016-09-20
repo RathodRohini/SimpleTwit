3eb7f4785ab5b947d6fe88670312372427d6a78c
Rohini Dinesh Rathod <rohiniddrathod@gmail.com>
2016-09-20 08:26:09 +0530

    Delete ItemInterface.java

--- ItemInterface.java
+++
@@ -1,8 +0,0 @@
-package subscription.interfaces;
-
-public interface ItemInterface {
-	public void setQuantity(long quantity);
-	public long getQuantity();
-	public void setUnit(String unit);
-	public String getUnit();
-}
