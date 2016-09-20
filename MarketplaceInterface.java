16549cc9ac5b422a618239ab001237275b801b8e
Rohini Dinesh Rathod <rohiniddrathod@gmail.com>
2016-09-20 08:26:48 +0530

    Delete MarketplaceInterface.java

--- MarketplaceInterface.java
+++
@@ -1,8 +0,0 @@
-package subscription.interfaces;
-
-public interface MarketplaceInterface {
-	public void setBaseURL(String baseURL);
-	public String getBaseURL();
-	public void setPartner(String partner);
-	public String getPartner();
-}
