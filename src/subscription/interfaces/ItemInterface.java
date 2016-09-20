1f4df79af7082ea3688ed446633ffb4966124fd9
Rohini Dinesh Rathod <rohiniddrathod@gmail.com>
2016-09-20 08:20:19 +0530

    Add files via upload

---
+++ src/subscription/interfaces/ItemInterface.java
@@ -0,0 +1,8 @@
+package subscription.interfaces;
+
+public interface ItemInterface {
+	public void setQuantity(long quantity);
+	public long getQuantity();
+	public void setUnit(String unit);
+	public String getUnit();
+}
